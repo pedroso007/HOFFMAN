008dee7e9e6529824d36cc9a068051c3d9539384
pedroso007 <102749965+pedroso007@users.noreply.github.com>
2024-07-26 19:23:37 -0300

    Add files via upload

--- nsei.js
+++ nsei.js
@@ -1,27 +1,48 @@
-const perguntas = [
-{
-enunciado: “Insira o enunciado da Pergunta 1”,
+let atual = 0;
+let perguntaAtual;
+
+const caixaPrincipal = document.querySelector(“.caixa-
+    principal”);
+    
+    const caixaPerguntas = document.querySelector(“.caixa-
+    perguntas”);
+    
+    const caixaAlternativas = document.querySelector(“.caixa-
+    alternativas”);
+    
+    const caixaResultado = document.querySelector(“.caixa-
+    resultado”);
+    
+    const textoResultado = document.querySelector(“.texto-
+    resultado”);
+    
+    const perguntas = [
+    {
+    enunciado: “Pergunta 1”,
+    alternativas: [“Alternativa 1”, “Alternativa 2”],
+    },
+    {
+    enunciado: “Pergunta 2”,
+    alternativas: [“Alternativa 1”, “Alternativa 2”],
+    },
+    ];
+
+    function mostraPergunta() {
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+        mostraPergunta();
+        }
+    
+    function mostraAlternativas() {
+        enunciado: “Assim que saiu da escola, você se
+depara com uma nova tecnologia: um chat que consegue
+responder a todas as dúvidas que uma pessoa pode ter.
+Além disso, o chat também gera imagens e áudios
+hiper-realistas. Qual o seu primeiro pensamento?”,
+
 alternativas: [
-{
-texto: “Alternativa 1”,
-afirmação: “Afirmação da alternativa 1”
-},
-{
-texto: “Alternativa 2”,
-afirmação: “Afirmação da alternativa 2”
-}
+“Isso é assustador!”,
+“Isso é maravilhoso!”
 ]
 }
-
-function mostraAlternativa() {
-for (const alternativa of perguntaAtual.alternativas) {
-const botaoAlternativas = document. createElement(“button”);
-botaoAlternativas.textContent = alternativa.texto;
-botaoAlternativas.addEventListener(“click”,
-function () {
-atual++;
-mostraPergunta();
-});
-caixaAlternativas.appendChild(botaoAlternativas);
-}
-}
+    
